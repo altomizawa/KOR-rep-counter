3636a8a65b290db54cb3f910ce7ea8cafc0afc32
altomizawa <alyssontomizawa@hotmail.com>
2023-02-23 23:57:22 -0300

    add button js

--- js/index.js
+++ js/index.js
@@ -39,3 +39,26 @@ function reset() {
 
 let catRxEl = document.getElementById("cat-rx-el")
 let catScaledEl = document.getElementById("cat-scaled-el")
+let rxDescriptionEl = document.getElementById("rx-description-el")
+console.log(rxDescriptionEl)
+
+function showRxWod() {
+    rxDescriptionEl.style.opacity = "1"
+    rxDescriptionEl.style.transitionDuration = "250ms"
+    catRxEl.style.color = "white"
+    catScaledEl.style.transitionDuration = "250ms"
+    catScaledEl.style.color = "black"
+    catScaledEl.style.setProperty("--scaled-button-color", "hsl(16, 100%, 100%)");
+    catRxEl.style.transitionDuration = "250ms"
+    catRxEl.style.setProperty("--rx-button-color", "hsl(16, 100%, 50%)");
+}
+
+function showScaledWod(){
+    rxDescriptionEl.style.opacity = "0";
+    catRxEl.style.color = "black"
+    catRxEl.style.transitionDuration = "250ms"
+    catScaledEl.style.color = "white"
+    catScaledEl.style.setProperty("--scaled-button-color", "hsl(16, 100%, 50%)");
+    catRxEl.style.setProperty("--rx-button-color", "hsl(16, 100%, 100%)");
+
+}
